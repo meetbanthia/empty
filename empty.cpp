f84727e4f8d9ff77433aa66b22623641594957fc
meetbanthia <80608233+meetbanthia@users.noreply.github.com>
2023-03-05 09:59:35 +0000

    First Commit

--- empty.cpp
+++ empty.cpp
@@ -1,1 +1,50 @@
 
+//WIN or LEARN, never LOSE!!!
+#include <bits/stdc++.h>
+using namespace std;
+
+//My Macros..
+#define int long long
+#define db double
+#define fl float
+#define endl "\n"
+#define spc " "
+#define rep(i,a,b,c) for(int i=a;i<b;i+=c)
+#define all(x) x.begin(), x.end()
+
+//For input output processes
+void MakeIOProcessFaster();
+void Input_Output_File();
+
+
+int32_t main()
+{
+MakeIOProcessFaster();
+Input_Output_File();
+
+int t;
+cin>>t;
+
+while(t--)
+{
+
+}
+return 0;
+}
+
+
+
+
+void MakeIOProcessFaster()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+}
+
+void Input_Output_File()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
