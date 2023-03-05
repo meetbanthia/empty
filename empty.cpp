4b98c3c6dce110148f3a930672e8e1d5ba296489
meetbanthia <80608233+meetbanthia@users.noreply.github.com>
2023-03-05 10:24:55 +0000

    Second Commit

--- empty.cpp
+++ empty.cpp
@@ -1,50 +1,45 @@
 
-//WIN or LEARN, never LOSE!!!
+// WIN or LEARN, never LOSE!!!
 #include <bits/stdc++.h>
 using namespace std;
 
-//My Macros..
+// My Macros..
 #define int long long
 #define db double
 #define fl float
 #define endl "\n"
 #define spc " "
-#define rep(i,a,b,c) for(int i=a;i<b;i+=c)
+#define rep(i, a, b, c) for (int i = a; i < b; i += c)
 #define all(x) x.begin(), x.end()
 
-//For input output processes
+// For input output processes
 void MakeIOProcessFaster();
 void Input_Output_File();
 
-
 int32_t main()
 {
-MakeIOProcessFaster();
-Input_Output_File();
+    MakeIOProcessFaster();
+    Input_Output_File();
 
-int t;
-cin>>t;
+    int t;
+    cin >> t;
 
-while(t--)
-{
-
+    while (t--)
+    {
+    }
+    return 0;
 }
-return 0;
-}
-
-
-
 
 void MakeIOProcessFaster()
 {
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 }
 
 void Input_Output_File()
 {
 #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 #endif
 }
